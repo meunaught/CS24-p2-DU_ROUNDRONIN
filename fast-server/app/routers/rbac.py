914e2b181c645987871e1d756cd8d1a5a7413d9f
ratingminus <4mahadi@gmail.com>
2024-03-30 23:34:06 +0600

    rbac started

---
+++ fast-server/app/routers/rbac.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from .. import models, schemas, utils
+from ..database import get_db
+
+router = APIRouter(
+    prefix="/rbac",
+    tags=['Role Based Access Control']
+)
