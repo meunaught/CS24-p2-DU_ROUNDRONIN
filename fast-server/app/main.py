51c116be3ecbd5cfd8335f06f8f07b75a18cfecf
ratingminus <4mahadi@gmail.com>
2024-03-30 18:13:56 +0600

    /users

---
+++ fast-server/app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from . import models
+from .database import engine
+from .routers import user
+from .config import settings
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(user.router)
+
+@app.get("/")
+def root():
+    return {"message": "Hello World!"}
--- fast-server/app/models.py
+++ fast-server/app/models.py
@@ -9,6 +9,6 @@ class User(Base):
     __tablename__= "users"
     id = Column(Integer, primary_key=True, index=True, nullable=False)
     email= Column(String, unique=True, nullable=False)
-    password_hash= Column(String, nullable=False)
-    last_login=Column(TIMESTAMP, server_default=text('now()'), nullable=False)
-    role= Column(String)
+    password= Column(String, nullable=False)
+    created_at=Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
+    role= Column(String, nullable=False, server_default='Unassigned')
