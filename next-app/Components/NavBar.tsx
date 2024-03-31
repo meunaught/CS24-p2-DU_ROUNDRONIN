d5856a19686f187010e97cc80c77a89c0d206b8e
Galib Mahmud Jim <galibmahmudjim@gmail.com>
2024-03-31 11:09:20 +0600

    Home

--- next-app/Components/NavBar.tsx
+++ next-app/Components/NavBar.tsx
@@ -5,17 +5,17 @@ import Button from "@mui/material/Button";
 
 const NavBar = () => {
       return (
-            <div className="h-[18vh] w-[100vw] border-b-[1px] border-[#ffffff35] z-[1000] fixed ">
+            <div className="h-[18vh] w-[100vw] top-0 border-b-[1px] bg-[#0f1521b6] border-[#ffffff35] z-[1000] fixed z-[1000]">
                   <div className="w-full h-full flex flex-row justify-center items-center">
                         <div className="w-[20%] pl-[2%] h-full flex flex-col justify-center items-center">
                               <img className="h-[80%] " src="/Image/logofigma.png" alt="logo" />
                         </div>
-                        <div className="w-[40%]  max-xl:w-[38%] h-full align-middle flex flex-col gap-1 justify-start mt-3 items-center">
-                              <div className=" font-rocknroll text-center text-white whitespace-nowrap  text-[30px] max-xl:text-[27px] max-lg:text-[25px]">Waste Management System</div>
-                              <div className="  font-rocknroll text-center text-white whitespace-nowrap text-[20px]">Dhaka North City Corporation</div>
+                        <div className="w-[40%]  max-xl:w-[38%] h-full align-middle flex flex-col gap-1 justify-start mt-[2%] items-center">
+                              <div className=" font-rocknroll text-center text-white whitespace-nowrap  text-[35px] max-xl:text-[30px] max-lg:text-[25px]">Waste Management System</div>
+                              <div className="  font-rocknroll text-center text-white whitespace-nowrap text-[25px] max-xl:text-[20px] max-lg:text-[18px]">Dhaka North City Corporation</div>
                         </div>
                         <div className="w-[40%] h-full flex flex-col justify-center items-center mr-5">
-                              <ul className="flex flex-row justify-end gap-10 text-white font-semibold text-[18px] items-center mr-[2%] text-[#ffffff89] no-underline">
+                              <ul className="w-full flex flex-row justify-around  text-white font-semibold text-[18px] items-center mr-[2%] text-[#ffffff89] no-underline">
                                     <li>
                                           <Link href="/#" className="text-[#ffffff89] no-underline">Home</Link>
                                     </li>
--- next-app/app/Home/page.tsx
+++ next-app/app/Home/page.tsx
@@ -2,11 +2,11 @@
 import './home.css'
 const Home = () => {
       return (
-            <div className="w-full h-[100vh] flex flex-row justify-center items-center pl-[10%] pr-[10%]">
-                  <div className="left w-[40%] h-full flex flex-col justify-center mt-[5%] items-start ">
+            <div className="w-full h-[100vh]  flex flex-row justify-center items-center pl-[10%] mt-[3%] pr-[10%]">
+                  <div className="left w-[40%] h-full flex flex-col justify-center  items-start ">
                         <img className="w-[80%]" src="/Image/homefig.svg" alt="Home Figure" />
                   </div>
-                  <div className="w-[60%] h-full flex flex-col justify-center mt-[5%] items-start ">
+                  <div className="w-[60%] h-full flex flex-col justify-center  items-start ">
                         <div className="right1 font-rocknroll text-white text-[50px] max-lg:text-[40px] whitespace-nowrap" >Welcome to EcoSync</div>
                         <div className=" right2 font-rocknroll mt-2 font-[200] text-[#ffffffd8] text-[30px] max-xl:text-[25px] max-lg:text-[20px] whitespace-nowrap" >Revolutionizing Dhaka's Waste Management</div>
                         <div className="right3 font-sans mt-5 font-[400] text-[#ffffffd8] text-[20px] max-lg:text-[20px] text-justify" >EcoSync revolutionizes Dhaka North City Corporation's waste management by using innovative technology to streamline waste collection, transportation, and processing. This comprehensive web application promotes a cleaner, greener Dhaka by enhancing operational efficiency and sustainability.</div>
