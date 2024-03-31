317ccd81f1adcf973afb30479d5596194c334769
ratingminus <4mahadi@gmail.com>
2024-03-31 19:36:21 +0600

    merged all api to backend

--- fast-server/app/database.py
+++ fast-server/app/database.py
@@ -14,13 +14,21 @@ initial_roles = [
     {"id":1003, "name":"Landfill Manager", "description":"Role for Landfill Managers"}
 ]
 
+initial_permissions = [
+    {"id":0, "name":"Admin Access", "description":"Full access to all resources"}
+]
+
+initial_users = [
+    {"id":0, "email":"admin@cs.com", "password":"cs", "role_id":1001}
+]
+
+initial_role_permissions = [
+    {"role_id":1001, "permission_id":0}
+]
+
 SQLALCHEMY_DATABASE_URL = settings.database_url
-
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 
@@ -43,6 +51,25 @@ def seed():
             db.add(db_role)
             db.commit()
         print("Roles seeded successfully")
+        for permission in initial_permissions:
+            db_permission = models.Permission(**permission)
+            if db.query(models.Permission).filter(models.Permission.id == db_permission.id).first():
+                continue
+            db.add(db_permission)
+            db.commit()
+        print("Permissions seeded successfully")
+        for user in initial_users:
+            db_user = models.User(**user)
+            if db.query(models.User).filter(models.User.id == db_user.id).first():
+                continue
+            db.add(db_user)
+            db.commit()
+        for role_permission in initial_role_permissions:
+            db_role_permission = models.role_permission_association(**role_permission)
+            if db.query(models.role_permission_association).filter(models.role_permission_association.role_id == db_role_permission.role_id).filter(models.role_permission_association.permission_id == db_role_permission.permission_id).first():
+                continue
+            db.add(db_role_permission)
+            db.commit()
     except Exception as error:
         print("Error seeding roles: ", error)
         db.rollback()
--- fast-server/app/models.py
+++ fast-server/app/models.py
@@ -4,24 +4,14 @@ from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
+# Define a many-to-many relationship table
 
-# Association table for many-to-many relationship between roles and permissions
-role_permission_association = Table('role_permission_association', Base.metadata,
-    Column('role_id', Integer, ForeignKey('roles.id')),
-    Column('permission_id', Integer, ForeignKey('permissions.id'))
-)
 class Role(Base):
     __tablename__ = 'roles'
 
     id = Column(Integer, primary_key=True, nullable='False')
     name = Column(String(50), nullable=False)
     description = Column(String(255))
-
-    # Define a relationship with the User table
-    users = relationship("User", back_populates="role")
-
-    # Define many-to-many relationship with permissions
-    permissions = relationship("Permission", secondary=role_permission_association, back_populates="roles")
 
     def __repr__(self):
         return f"<Role(name='{self.name}', description='{self.description}')>"
@@ -35,7 +25,6 @@ class User(Base):
     created_at=Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
 
     role_id = Column(Integer, ForeignKey('roles.id'), server_default='1000', nullable=False)
-    role = relationship("Role", back_populates="users")
 
     def __repr__(self):
         return f"<User(username='{self.username}')>"
@@ -47,8 +36,15 @@ class Permission(Base):
     name = Column(String(50), nullable=False)
     description = Column(String(255))
 
-    # Define many-to-many relationship with roles
-    roles = relationship("Role", secondary=role_permission_association, back_populates="permissions")
     def __repr__(self):
         return f"<Permission(name='{self.name}', description='{self.description}')>"
 
+class role_permission_association(Base):
+    __tablename__ = 'role_permission_association'
+
+    uuid = Column(Integer, primary_key=True)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+    permission_id = Column(Integer, ForeignKey('permissions.id'))
+
+    def __repr__(self):
+        return f"<RolePermission(role_id='{self.role_id}', permission_id='{self.permission_id}')>"
