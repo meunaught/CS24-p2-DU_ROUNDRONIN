8ed6ca8358278f385c8a8bb3d39af3e4d451627a
Galib Mahmud Jim <galibmahmudjim@gmail.com>
2024-04-01 10:21:24 +0600

    create user

--- next-app/app/admin/Usermanagement/page.tsx
+++ next-app/app/admin/Usermanagement/page.tsx
@@ -11,33 +11,42 @@ import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import "./user.css"
+//............
+
+
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+//............
+
+const role = ['Unassigned', 'Admin', 'STS Manager', 'Landfill Manager'
+
+];
 
 interface User {
       id: number;
       name: string;
       email: string;
-      phone: string;
+      phone_number: string;
+      role_id: number;
 }
 const UserList = (props: { users: User[] }) => {
       return (
-            <div className="w-full  min-h-screen flex flex-col justify-center items-center z-10 pt-[20%] ">
-                  <div className={`w-full  h-full  m-10  bottom-1  min-w-[300px]  py[200px] flex flex-col justify-center items-center pt-[2%] `}>
 
-                        <div className="text-white font-rocknroll text-[30px]  font-bold">
-                              Users
-                        </div>
-                        <div className="w-[400px] mt-[30px] flex flex-col text-[#ffffff] font-bold text-[18px] justify-center items-start">
-                              {props.users.map((user) => {
-                                    return (
-                                          <div key={user.id} className="w-full border-[#ffffff09] border-b-[1px] flex flex-row justify-between items-center py-[10px]">
-                                                <div className="text-[#ffffff]">{user.name}</div>
-                                                <div className="text-[#ffffff]">{user.email}</div>
-                                                <div className="text-[#ffffff]">{user.phone}</div>
-                                          </div>
-                                    )
-                              })}
-                        </div>
-                  </div>
+
+            <div className="w-full h-full px-[1%] mt-[30px] flex flex-col text-[#ffffff]  font-rocknroll font-bold text-[18px] justify-center items-start gap-3 mb-5 ">
+                  {props.users.map((user) => {
+                        return (
+                              <div key={user.id} className="w-full bg-[#110a2bd8] shadow-md  border-[#ffffff09] p-[3%] py-4 rounded-sm  border-2 flex flex-row justify-between items-center py-[10px] cursor-pointer">
+                                    <div className="text-[#ffffff] text-justify w-full">{user.name}</div>
+                                    <div className="text-[#ffffff] text-justify w-full">{user.email}</div>
+                                    <div className="text-[#ffffff] w-full text-right">{role[user.role_id - 1000]}</div>
+                              </div>
+                        )
+                  })}
             </div>
       )
 
@@ -93,15 +102,16 @@ const Usermanage = () => {
 
 
       return (
-            <div className="w-full h-full flex flex-col justify-center items-center ml-[5%]">
+            <div className="w-full h-fit flex flex-col justify-center items-center ml-[5%]">
                   <ToastContainer />
-                  <div className="w-full  min-h-screen flex flex-col justify-center items-center z-10 pt-[20%] ">
-                        <div className={`w-[60%]  h-full  bg-[#ffffff09] rounded-xl m-10  bottom-1 border-white min-w-[300px] backdrop-blur-lg  py[200px] flex flex-col justify-center items-center pt-[2%] `}>
+                  <div className=" w-full h-fit flex flex-col justify-center items-center z-10 pt-[20%] ">
 
-                              <div className="text-white font-rocknroll text-[30px]  font-bold">
+                        <div className="w-[70%] h-fit ml-[10%]  h-full  bg-[#ffffff09] rounded-xl m-10  border-white min-w-[300px] backdrop-blur-lg   flex flex-col justify-start items-center pt-[2%] mt-[10%] ">
+
+                              <div className="text-white font-rocknroll text-[30px] mt-5  font-bold">
                                     Users
                               </div>
-                              <div className="w-[400px] mt-[30px] flex flex-col text-[#ffffff] font-bold text-[18px] justify-center items-start">
+                              <div className="w-full h-full mt-[30px] flex flex-col text-[#ffffff] font-bold text-[18px] justify-center items-start  mb-[5%]">
                                     <UserList users={users} />
                               </div>
                         </div>
